a27f0ba3d84bddfe36b4aa5692d0c14577503826
ali1naqvi <70173682+ali1naqvi@users.noreply.github.com>
2021-03-11 00:20:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function sendMail(params) {
     var tempParams = {
-        from_name: document.getElementById("fromName").value,
         to_name: document.getElementById("toName").value,
+        from_name: document.getElementById("fromName").value,
         message: document.getElementById("msg").value,
     };
 
     emailjs.send('service_ijmvzpf','template_rbt5n0h',tempParams)
     .then(function(res){
-        console.log("success",res.status)
+        console.log("success", res.status);
     })
 }
